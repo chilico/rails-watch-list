7e60fcf0e7f5cf8c3b1e190b710da94e18ff57d8
chris owen <farawaychris.owen@gmail.com>
2022-05-26 14:04:30 +0100

    seeded movie db from tmdb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database ...'
+Movie.destroy_all
+
+puts 'Creating movies ...'
+
+# through Faker
+# 20.times do
+#   title = Faker::Movie.title
+#   overview = Faker::Movie.quote
+#   poster_url = Faker::Fillmurray.image
+#   rating = rand(0.0..10.0).round(1)
+#   attributes = { title: title, overview: overview, poster_url: poster_url, rating: rating }
+#   movie = Movie.create(attributes)
+#   puts "Created #{movie.title}"
+# end
+
+# through The Movie Database (Le Wagon API)
+url = "http://tmdb.lewagon.com/movie/top_rated"
+raw = JSON.parse(URI.open(url).read).transform_keys(&:to_sym)
+
+(0..19).each do |idx|
+  title = raw[:results][idx]["title"]
+  overview = raw[:results][idx]["overview"]
+  rating = raw[:results][idx]["vote_average"]
+  img_path = raw[:results][idx]["poster_path"]
+  poster_url = "https://image.tmdb.org/t/p/w500#{img_path}"
+  attributes = { title: title, overview: overview, poster_url: poster_url, rating: rating }
+  movie = Movie.create(attributes)
+  puts "Created #{movie.title}"
+end
+
+puts "Finished!"
