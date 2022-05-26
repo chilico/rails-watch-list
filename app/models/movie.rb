fb244143492d49ab767d8aee440df5b8b0934fa8
chris owen <farawaychris.owen@gmail.com>
2022-05-26 12:26:48 +0100

    correction to validates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_many :lists, through: :bookmarks
 
   validates :title, presence: true, allow_blank: false, uniqueness: true
   validates :overview, presence: true, allow_blank: false
